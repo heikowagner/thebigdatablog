30c04e35ac641415e4bb5cd0f98bd2e4aa294780
Heiko Wagner <heiko.wagner@record-evolution.de>
2019-04-14 18:26:57 +0200

    anpassung revenue

--- js_ai_project/js_bandit/bandit.js
+++ js_ai_project/js_bandit/bandit.js
@@ -1,47 +1,38 @@
 /*
 --Bandit.js
 -This Program computes an optimal lever and average return of the multiarmed bandit problem
-
 --Input
-f=[f_1(x),...,f_K(x)]       -An Array of size K containig functions given a cetrain reward r eg. binomial or a normal distribution with different means
-T                           -Runs to retrieve the decison (the first, t or  N  will terminate the algorithm)
-
+f=[f_1(x),...,f_K(x)]       -An Array of size K containing functions given a certain reward r eg. binomial or a normal distribution with different means
+T                           -Runs to retrieve the decision (the first, t or  N  will terminate the algorithm)
 --Output
-d                           -A number d=(1,...,K) given the estimated best leaver
-r                           -the average reward from that leaver
-
+sum \mu                     -the average revenue
+x_out
 --Heiko Wagner 2019
 */
-
+ 
 bandit = function(f, T) {
-    var K = f.length;
     //pull each lever once
     var x_out = f.map((x) => [x(), 1])
     var a;
     for (var t = 0; t < T; t++) {
         //determine the position with the highest value
         var j = x_out.map((x) => x[0] / x[1] + Math.sqrt((2 * Math.log(t)) / x[1]))
-        a = j.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+        a = j.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0)
         //pull maximum lever
         x_out[a] = [x_out[a][0] + f[a](), x_out[a][1] + 1]
     }
-    return [a, x_out[a][0] / x_out[a][1]]
+    return [x_out.map((x)=>x[0]).reduce( (a,b) =>a+b)/T, x_out]
 }
-
-
-
+ 
 //Example
 var K = 10
-
+ 
 var f = []
 for (var k = 0; k < K; k++) {
     f.push(eval('() => Math.random()*' + k))
 }
-
-//The Bandit should return a=9 and an average return of approx 4.5
+ 
 bandit(f, 10000)
-
-
 
 //Let's define a Casino Class
 class Casino {
@@ -57,16 +48,15 @@ class Casino {
         return f
     }
 }
-
+ 
 var K_dash = 5
 var K = 10
-
+ 
 //To build a two stage bandit problem we build 
-
+ 
 var f_2 = []
 for (var k = 1; k <= K_dash; k++) {
-    f_2.push(eval('() => { return bandit(new Casino(' + K + ',' + k + ').levers() ,1000)[1] }'))
+    f_2.push(eval('() => { return bandit(new Casino(' + K + ',' + k + ').levers() ,1000)[0] }'))
 }
-
-//The Bandit should return a=4  and an average return of approx 20
+ 
 bandit(f_2, 1000)
