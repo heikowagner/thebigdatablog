490027cb9b4d8b520406de384c1c44b5709e700c
Heiko Wagner <heikowagner@t-online.de>
2023-01-22 23:37:40 +0100

    cleanup
    cleanup

--- transformer/load.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-url = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt"
-
-df = pd.read_csv(url)
